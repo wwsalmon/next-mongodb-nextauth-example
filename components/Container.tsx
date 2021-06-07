586d47529f6abcb787348f2fc36f97dc31e84831
Samson Zhang <wwsamson@yahoo.com>
2021-06-07 10:22:30 -0700

    fix spacing for classes on container component

--- components/Container.tsx
+++ components/Container.tsx
@@ -4,9 +4,9 @@ export default function Container({children, className, width}: { children: Reac
     return (
         <div
             className={"mx-auto px-4 " + ({
-                "4xl": "max-w-4xl",
-                "7xl": "max-w-7xl",
-                "full": ""
+                "4xl": "max-w-4xl ",
+                "7xl": "max-w-7xl ",
+                "full": " "
             }[width]) + (className || "")}
         >
             {children}
