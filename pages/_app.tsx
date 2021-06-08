0719be444c570693a0c647493fde112f68bef377
Samson Zhang <wwsamson@yahoo.com>
2021-06-07 18:41:11 -0700

    add loading bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,16 @@ import "../styles/globals.css";
 import {Provider} from "next-auth/client";
 import ReactModal from "react-modal";
 import Navbar from "../components/Navbar";
+import NProgress from "nprogress";
+import "../styles/nprogress.css";
+import Router from "next/router";
+
+Router.events.on("routeChangeStart", (url) => {
+    console.log(`Loading: ${url}`)
+    NProgress.start()
+});
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 export default function App({Component, pageProps}) {
     return (
