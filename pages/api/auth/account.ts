bd30a95e9593a22f35a026f51b45f29d14a11c6a
Samson Zhang <wwsamson@yahoo.com>
2021-07-26 17:54:59 -0700

    add send messages to account api endpoint

--- pages/api/auth/account.ts
+++ pages/api/auth/account.ts
@@ -7,11 +7,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     switch (req.method) {
         case "POST":
             const session = await getSession({req});
-            if (!session) return res.status(403);
+            if (!session) return res.status(403).send("Unauthed");
             if (session.userId) return res.status(200).json({message: "Account already exists"});
 
             if (!(req.body.username)) {
-                return res.status(406);
+                return res.status(400).send("Missing username");
             }
 
             try {
@@ -29,6 +29,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                 return res.status(500).json({message: e});
             }
         default:
-            return res.status(405);
+            return res.status(405).send("Invalid method");
     }
 }
