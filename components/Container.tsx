171535ce8ad82721f6fc91905edd826964afd2e8
Samson Zhang <wwsamson@yahoo.com>
2021-09-17 16:28:36 -0700

    add and update components

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,14 +1,18 @@
 import {ReactNode} from "react";
 
-export default function Container({children, className, width = "4xl", padding = 4}: { children: ReactNode, className?: string, width?: "4xl" | "7xl" | "full", padding?: 4 | 6 | 8 }) {
+export default function Container({children, className, width = "4xl", padding = 4}: { children: ReactNode, className?: string, width?: "3xl" | "4xl" | "5xl" | "7xl" | "full", padding?: 4 | 6 | 8 | 10 | 12 }) {
     return (
         <div
             className={"mx-auto px-4 " + ({
-                4: "px-4 ",
-                6: "px-6 ",
-                8: "px-8 ",
+                4: "",
+                6: "sm:px-6 ",
+                8: "sm:px-8 ",
+                10: "sm:px-10 ",
+                12: "sm:px-12 ",
             }[padding]) + ({
+                "3xl": "max-w-3xl ",
                 "4xl": "max-w-4xl ",
+                "5xl": "max-w-5xl ",
                 "7xl": "max-w-7xl ",
                 "full": " "
             }[width]) + (className || "")}
--- components/SignInButton.tsx
+++ components/SignInButton.tsx
@@ -1,4 +1,4 @@
-import Button from "./Button";
+import Button from "./headless/Button";
 import {FaGoogle} from "react-icons/fa";
 import {signIn} from "next-auth/client";
 
